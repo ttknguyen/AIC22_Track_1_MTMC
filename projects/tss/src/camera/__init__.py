fa234f31b978c13cfd9934115432140697f14e99
DuongTran1708 <duongtran.k10@gmail.com>
2022-04-19 13:58:00 +0900

    Edit camera.

--- projects/tss/src/camera/__init__.py
+++ projects/tss/src/camera/__init__.py
@@ -6,9 +6,6 @@
 
 from __future__ import annotations
 
-from .aic_vehicle_counting_camera import *
-from .aic_vehicle_counting_camera_multithread import *
 from .aic_mtmc_camera import *
-from .aic_product_counting_camera import *
 from .moi import *
 from .roi import *
