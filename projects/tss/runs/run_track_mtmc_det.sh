b5a554948ef6cd6eebc58a9d75bcdf7214332b00
DuongTran1708 <duongtran.k10@gmail.com>
2022-04-19 15:10:35 +0900

    Update run file.

--- projects/tss/runs/run_track_mtmc_det.sh
+++ projects/tss/runs/run_track_mtmc_det.sh
@@ -8,28 +8,6 @@ ROOT_DIR=$(dirname "${ROOT_DIR}")
 ROOT_DIR=$(dirname "${ROOT_DIR}")
 ROOT_DIR=$(dirname "${ROOT_DIR}")
 
-# seqs1=(c041)
-# img_size=1280
-# conf_thres=0.1
-# gpu_id=1
- # shellcheck disable=SC2068
-# for seq in ${seqs1[@]}
-# do
-#     CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/yolov5v4/detect.py  \
-#         --name   ${seq}  \
-# 		--output ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/  \
-# 		--source ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/images/${seq}    \
-# 		--weights ${ROOT_DIR}/models_zoo/pretrained/detector/yolov5x.pt  \
-# 		--roi_region ${ROOT_DIR}/projects/tss/data/aic22_mtmc/dataset_a/${seq}/roi.jpg \
-# 		--conf 0.1 \
-# 		--agnostic  \
-# 		--save-txt  \
-# 		--save-conf  \
-# 		--classes 2 5 7 \
-# 		--img-size 1280 &
-# done
-
-
 seqs1=(c041 c043 c045)
 img_size=1280
 conf_thres=0.1
@@ -37,7 +15,7 @@ gpu_id=1
 # shellcheck disable=SC2068
 for seq in ${seqs1[@]}
 do
-    CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/ScaledYOLOv4/detect.py  \
+    CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/yolov5v4/detect.py  \
         --name   ${seq}  \
 		--output ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/  \
 		--source ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/images/${seq}  \
@@ -56,7 +34,7 @@ gpu_id=0
 # shellcheck disable=SC2068
 for seq in ${seqs2[@]}
 do
-    CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/ScaledYOLOv4/detect.py \
+    CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/yolov5v4/detect.py \
         --name   ${seq}  \
 		--output ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/  \
 		--source ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/images/${seq}  \
@@ -70,34 +48,5 @@ do
 		--img-size ${img_size} &
 done
 
-#gpu_id=3
-#seq=c045
-#CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/yolov5v4/detect.py  \
-#        --name   ${seq}  \
-#		--output ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/  \
-#		--source ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/images/${seq}  \
-#		--weights ${ROOT_DIR}/models_zoo/pretrained/detector/yolov5x.pt  \
-#		--roi_region ${ROOT_DIR}/projects/tss/data/aic22_mtmc/dataset_a/${seq}/roi.jpg \
-#		--conf ${conf_thres}  \
-#		--agnostic  \
-#		--save-txt  \
-#		--save-conf  \
-#		--classes 2 5 7 \
-#		--img-size ${img_size} &
-#
-#gpu_id=2
-#seq=c046
-#CUDA_VISIBLE_DEVICES=${gpu_id} python3 projects/tss/src/detectors/yolov5v4/detect.py  \
-#        --name   ${seq}  \
-#		--output ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/  \
-#		--source ${ROOT_DIR}/projects/tss/data/aic22_mtmc/outputs/images/${seq}  \
-#		--weights ${ROOT_DIR}/models_zoo/pretrained/detector/yolov5x.pt  \
-#		--roi_region ${ROOT_DIR}/projects/tss/data/aic22_mtmc/dataset_a/${seq}/roi.jpg \
-#		--conf ${conf_thres}  \
-#		--agnostic  \
-#		--save-txt  \
-#		--save-conf  \
-#		--classes 2 5 7 \
-#		--img-size ${img_size}
 
 wait
